ce3b97503ccad664dda4e3201d2d4e60cbada938
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-01 16:36:33 -0600

    Add form User

---
+++ Obra/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Usuario  # Importa tu modelo de usuario personalizado
+
+#! Usuario registro
+
+
+class RegistroForm(UserCreationForm):
+    class Meta:
+        model = Usuario  # * Asociamos el formulario con el modelo de usuario personalizado
+        fields = ('email', 'password1', 'password2', 'first_name',
+                  'last_name', 'rol')  # * campos requeridos
