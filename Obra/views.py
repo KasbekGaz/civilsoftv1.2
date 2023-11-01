fe7ca10e58d12a60c5f75cf6ef4bd682e034b33c
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-01 17:52:15 -0600

    Add Register User View

--- Obra/views.py
+++ Obra/views.py
@@ -1,20 +1,36 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
+from django.shortcuts import redirect, render
 # * Arriba cosas para la vista personalizada
 # * modelos
-from .models import Usuario, Obra, Tarea, Gasto, Galeria, Volumen
+from .models import Obra, Tarea, Gasto, Galeria, Volumen
 # * serializadores
-from .serializers import UsuarioSerializer, ObraSerializer, TareaSerializer, GastoSerializer, GaleriaSerializer, VolumenSerializer
+from .serializers import ObraSerializer, TareaSerializer, GastoSerializer, GaleriaSerializer, VolumenSerializer
 # * Decorators para la gestion del rol
 from .decorators import es_administrador, es_consultor
-
+# * Manejo de sesiones Usuario
+from django.contrib.auth import login, logout
+from .forms import RegistroForm
 # Create your views here.
 
+
+def registrar_usuario(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistroForm()
+    return render(request, 'registro.html', {'form': form})
+
+
+''' El serializador del modelo usuario anterior
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+'''
 
 
 class ObraViewSet(viewsets.ModelViewSet):
