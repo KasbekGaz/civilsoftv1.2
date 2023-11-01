43999573390ee739216eacf2d284683d663d521c
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-01 16:19:32 -0600

    Add Model User

--- Obra/models.py
+++ Obra/models.py
@@ -1,32 +1,48 @@
 from django.utils import timezone
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
 
 
 #! modelo de Usuario
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El email es Obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class Usuario(models.Model):
-    ROLES = (
-        ('Administrador', 'Administrador'),
-        ('Consultor', 'Consultor')
-    )
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('El superusuario debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('El superusuario debe tener is_superuser=True.')
 
-    nombre = models.CharField(max_length=100)
-    correo = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    confirm_password = models.CharField(max_length=100, null=True)
-    rol = models.CharField(max_length=15, choices=ROLES)
-    # * relacion de muchos a muchos de la tabla usuario a obras
-    obras = models.ManyToManyField('Obra', related_name='usuarios')
+        return self.create_user(email, password, **extra_fields)
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return f"{self.nombre} ({self.rol})"
+        return self.email
 
 
 #! Modelo Obra
-
-
 class Obra(models.Model):
 
     nombre = models.CharField(max_length=255)
