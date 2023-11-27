4db87aeba648ba6418d5bb431ec92076b6d601f0
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-27 17:25:35 -0600

    Add peticiones User

--- client/src/models/ApiBackend.js
+++ client/src/models/ApiBackend.js
@@ -2,36 +2,97 @@ import Cookies from 'js-cookies'
 import axios from 'axios'
 
 const apiUrlBase = 'http://127.0.0.1:8000'
-const token = Cookies.get('loggedToken')
+
+
+
 const apiConfig ={
     headers: {
         'Content-Type': 'application/json'
     },
-    headersToken: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${token}`
+};
+
+const addTokenToHeaders = () => {
+    const token = Cookies.get('loggedToken');
+    if(token){
+        apiConfig.headersToken = {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${token}`,
+        }
     }
-}
+};
 
 
 
+
+//! Usuario peticiones --------
+//* Login
 export const loginUser = async (username, password) =>{
     try {
-        const apiUrl= `${apiUrlBase}/app/api/v1/login/`
-        const response = await axios.post(apiUrl, {
-            headers: apiConfig.headers,
-            body: JSON.stringify({username,password})
-        })
-
-
-        if(!response.ok){
-            throw new Error('Error al iniciar Sesión !!')
+        addTokenToHeaders();
+    
+        const apiUrl = `${apiUrlBase}/app/api/v1/login/`;
+        const response = await axios.post(apiUrl, { username, password }, apiConfig.headers);
+    
+        if (!response.ok) {
+          throw new Error('Error al iniciar Sesión !!');
         }
-        const responseData = await response.json()
-        return responseData
-    }catch (error){
-        console.error('Error al iniciar sesión:', error.message)
+        const responseData = await response.data;
+        return responseData;
+      } catch (error) {
+        console.error('Error al iniciar sesión:', error.message);
+        throw error;
+      }
+};
+//* Register
+export const registerUer = async userData =>{
+    try{
+        const apiUrl = `${apiUrlBase}/app/api/v1/register/`
+        const response = await axios.post(apiUrl, userData,apiConfig.headers );
+        console.log('Usuario registrado con éxito:', response.data);
+    }catch(error){
+        console.error('Error al registrarse', error.message)
         throw error
     }
+};
+//* Logout
+export const logoutUser = async () =>{
+    try{
+        const apiUrl = `${apiUrlBase}/app/api/v1/logout/`;
+        const response = await axios.delete(apiUrl, apiConfig.headersToken);
 
+        if(!response.ok){
+            throw new Error('Error al cerrar sesion!!');
+        }
+
+        console.log('Sesión cerrada con éxito:', response.data);
+    }catch(error){
+        console.error('Error al cerrar sesion:', error.message);
+        throw error;
+    }
+};
+
+
+
+//! Modelo de Obra ------------------------------
+//* LISTAR OBRAS
+export const listObra = async () =>{
+    try{
+        addTokenToHeaders();
+        const apiUrl = `${apiUrlBase}/app/api/v1/obras/`;
+        const response = await axios.get(apiUrl, apiConfig.headersToken);
+
+        if(!response.ok){
+            throw new Error('Error al obtener obras !!')
+        }
+
+        const responseData = await response.data;
+        return responseData;
+
+
+    }catch(error){
+        console.error('Error al obtener la lista de obras.', error.message);
+        throw error;
+    }
 }
+//* CREAR OBRAS
+
