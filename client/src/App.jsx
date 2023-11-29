02c41475f701c468f2aebf75b44bb77d6aca2d78
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-11-29 14:16:50 -0600

    Config New structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,13 @@
 import './App.css'
-import { useState, useEffect } from 'react';
-//! Paginas
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import RegisterView from "./pages/RegisterView";
-import { LoginView } from "./pages/LoginView";
-import  ObraForm  from "./pages/ObraForm";
-import Plantillas from './pages/Plantillas';
-//! componentes
-import ObraList from './components/ObraList';
-import Sidebar from './components/Sidebar';
+import React from 'react';
+import Home from './components/containers/Home'
 
 
 function App() {
   return (
-    <>
-    {/* Esto es para navegar entre lasdpaginas   */}
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to ="/login"/>} />
-        <Route path="/register" element={<RegisterView/>} />
-        <Route path="/login" element={<LoginView/>} />
-        <Route path="/obras" element={<ObraList/>} />
-        <Route path="/create-obra" element={<ObraForm/>} />
-        <Route path="/plantilla" element={<Plantillas/>} />
-      </Routes>
-    </BrowserRouter>
-    </>
+    <div>
+      <Home />
+    </div>
   )
 }
 
