19e7fd7f4be16cd5b9cdb6c99565bd2696680c6b
Oswald99 <oswaldlazarof.99@gmail.com>
2023-12-11 17:35:25 -0600

    Delete Front resources

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
-import './App.css'
-import React from 'react';
-import Home from './components/containers/Home'
 
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <Router>
+        <PublicRoutes />
+    </Router>
+  );
 }
 
-export default App
+export default App;
