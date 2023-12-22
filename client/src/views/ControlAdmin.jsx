ced1a72a5bafe94e6597c1b0f1c364d80e1d1331
Oswald99 <oswaldlazarof.99@gmail.com>
2023-12-22 14:56:30 -0600

    Probando peticion CreateGastobyObra

--- client/src/views/ControlAdmin.jsx
+++ client/src/views/ControlAdmin.jsx
@@ -7,7 +7,7 @@ import APIbackend from "../api/APIbackend";
 const ControlAdmin = () => {
     const { id } = useParams();
     const [obraData, setObraData] = useState({});
-    const [ obraId = id, setObraId] = useState({});
+
 
     const navigate = useNavigate();
 
@@ -16,7 +16,6 @@ const ControlAdmin = () => {
             console.log(id);
             const obraData = await APIbackend.getObraById(id);
             setObraData(obraData);
-            setObraId(id);
 
         }catch(error){
             console.error('Error al obtener datos.', error.message);
@@ -29,7 +28,7 @@ const ControlAdmin = () => {
     };
     //! Para el formulario se usa esto--------------------
     const [gastoData, setGastoData] =useState({
-        obra: obraId,
+        obra: id,
         fecha: '',
         proveedor: '',
         concepto: '',
@@ -39,7 +38,6 @@ const ControlAdmin = () => {
         Tipo: 'Efectivo',
         importe: '0',
     });
-
     const handleInputChange = (e) =>{
         const { name, value } = e.target;
         setGastoData((prevData) => ({
@@ -51,12 +49,12 @@ const ControlAdmin = () => {
     const handleCreateGasto = async () => {
         try{
             console.log(gastoData);
-            const response = await APIbackend.createGastobyObra(obraId, gastoData);
+            const response = await APIbackend.createGastobyObra(id, gastoData);
             console.log('Gasto creado: ', response);
             alert('Gasto agregado correctamente!');
             //*Para limpiar el formulario----
             setGastoData({
-                obra: obraId,
+                obra: id,
                 fecha: '',
                 proveedor: '',
                 concepto: '',
@@ -89,6 +87,98 @@ const ControlAdmin = () => {
 
             <div class="Formulario">
                 <form className="flex flex-col justify-center items-center">
+                    <label className="mb-4">
+                    Fecha :
+                        <input
+                            type="date"
+                            name="fecha"
+                            value={gastoData.fecha}
+                            onChange={handleInputChange}
+                            className="ml-4"
+                        />
+                    </label>
+
+                    <label className="mb-4">
+                        Proveedor :
+                        <input 
+                            type="text" 
+                            name="proveedor"
+                            value={gastoData.proveedor}
+                            onChange={handleInputChange}
+                            className="ml-4"/>
+                    </label>
+
+                    <label className="mb-4">
+                        Concepto :
+                        <input 
+                            type="text" 
+                            name="concepto"
+                            value={gastoData.concepto}
+                            onChange={handleInputChange}
+                            className="ml-4"/>
+                    </label>
+
+                    <label className="mb-4">
+                        Descripcion :
+                        <input 
+                            type="text" 
+                            name="descripcion"
+                            value={gastoData.descripcion}
+                            onChange={handleInputChange}
+                            className="ml-4"/>
+                    </label>
+
+                    <label className="mb-4">
+                        Categoria del Gasto :
+                        <select
+                            name="categoria"
+                            value={gastoData.categoria}
+                            onChange={handleInputChange}>
+                                <option value="Administracion">Administración</option>
+                                <option value="Mano de obra">Mano de Obra</option>
+                                <option value="Materiales">Materiales</option>
+                                <option value="Viaticos">Viáticos</option>
+                                <option value="Varios">Varios</option>
+                        </select>
+                    </label>
+
+                    <label className="mb-4">
+                        Facturado :
+                        <select 
+                            name="facturado"
+                            value={gastoData.facturado}
+                            onChange={handleInputChange}>
+                                <option value="Facturado">Facturado</option>
+                                <option value="No Facturado"> No Facturado</option>
+                            </select>
+                    </label>
+
+                    <label className="mb-4">
+                        Tipo de Gasto :
+                        <select 
+                            name="Tipo"
+                            value={gastoData.Tipo}
+                            onChange={handleInputChange}>
+                                <option value="Efectivo">Efectivo</option>
+                                <option value="Transferencia">Transferencia</option>
+                            </select>
+                    </label>
+
+                    <label className="mb-4">
+                        Importe $ :
+                        <input 
+                            type="number"
+                            name="importe"
+                            value={gastoData.importe || ''}
+                            onChange={handleInputChange}/>
+                    </label>
+
+                    <button
+                        className="bg-green-400"
+                        type="button"
+                        onClick={handleCreateGasto}>
+                        Agregar Nuevo Gasto
+                    </button>
 
                 </form>
             </div>
