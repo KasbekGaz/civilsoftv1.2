f3989a70d46ab120c8d56be084d06d5572e83af8
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-12-29 20:58:46 -0600

    Fix errores en esta vista CRUD de Tarea

--- client/src/views/TareaView.jsx
+++ client/src/views/TareaView.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import TareaList from "../components/Tarea/TareaList";
 import APIbackend from "../api/APIbackend";
-import TareaForm from "../components/Tarea/TareaForm";
+
 
 const TareaView = () => {
-    const { id } = useParams();
+    const { id } = useParams();//* id de obra
     const [obraData, setObraData] = useState({});
-    const [obraId, setObraId] = useState(null);
+    const [tareas, setTareas] = useState([]);
 
     const navigate = useNavigate();
 
@@ -18,8 +17,6 @@ const TareaView = () => {
             const obraData = await APIbackend.getObraById(id);
             setObraData(obraData);
 
-            setObraId(id);
-
             console.log(id);
             
         } catch (error) {
@@ -27,17 +24,85 @@ const TareaView = () => {
         }
     };
 
-
-
     const handleBack = () => {
-        navigate(`/dashboard`)
+        console.log('Id de obra:', id);
+        navigate(`/details-obra/${id}`);
     };
+//! Para el formulario de Tareas --------------------
+const [tareaData, setTareaData] = useState({
+    Fvence: '',
+    Fcreado: '',
+    Fcompletado: null,
+    titulo: '',
+    descripcion: '',
+    estado: 'no_completado',
+    obra: id,
+});
+
+const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTareaData((prevData) => ({
+        ...prevData,
+        [name]: value,
+    }));
+};
+
+const handleCreateTarea = async () => {
+    try {
+        console.log(tareaData);
+        
+        const response = await APIbackend.createTareabyObra(id, tareaData);
+        
+        console.log('Tarea creada: ', response);
+
+        alert('Tarea agregada correctamente !! ');
+        //* Limpiar el formulario ---
+        setTareaData({
+            Fvence: '',
+            Fcreado: '',
+            Fcompletado: null,
+            titulo: '',
+            descripcion: '',
+            estado: 'no_completado',
+            obra: id,
+        });
+        fetchTareas();
+    } catch (error) {
+        console.error('Error al crear la TAREA', error.message);
+    }
+};
 
     useEffect(() => {
-        fetchObraDetails();
+        fetchObraDetails(); //* Obtener datos de obra
+        fetchTareas(); //* cargar los datos.
     }, [id]);
-
-
+//! Para listar las tareas por id de Obra
+const fetchTareas = async () =>{
+    try{
+        console.log(id); //obra_id
+        const tareaData = await APIbackend.listTareaByObra(id);
+        setTareas(tareaData);
+        console.log('id:', id, 'Datos Tarea:', tareaData)
+    }catch(error){
+        console.error('Error al listar tareas.', error.message)
+    }
+};
+//! Para actualizar una tarea por su id
+const handleActualizar = (id, tareaId) => {
+    console.log('Datos que entran: ', id, tareaId);
+    navigate(`/update-tarea-by-obra/${id}/${tareaId}`)
+};
+//! Para Eliminar una tarea por su id
+const handleEliminar = async (id, tareaId) => {
+    try{
+        console.log('Datos que entran: ', id, tareaId);
+        await APIbackend.deleteTareaForObra(id, tareaId);
+        alert('Eliminaste la tarea con exito !!');
+        fetchTareas();
+    }catch(error){
+        console.log('Error al eliminar el gasto', error.message);
+    }
+};
     return (
         <div>
             <h1>Proyecto "{obraData.nombre}"</h1>
@@ -45,14 +110,114 @@ const TareaView = () => {
             <h2>Control de Tareas:</h2>
             
             <button className="bg-green-400 py-2 px-4 mb-4" onClick={handleBack}>
-                Regresar Al Inicio
+                Regresar a las Acciones
             </button>
-
+            <h1>Agregar una nueva Tarea: </h1>
             <div>
-            {obraId && <TareaForm obraId={obraId} />}
+                <form  className="flex flex-col justify-center items-center">
+                    <label className="mb-4">
+                        Fecha Vencimiento:
+                        <input
+                            type="date"
+                            name="Fvence"
+                            value={tareaData.Fvence}
+                            onChange={handleInputChange}
+                            className="ml-4"
+                        />
+                    </label>
+
+                    <label className="mb-4">
+                        Fecha creada en:
+                        <input
+                            type="date"
+                            name="Fcreado"
+                            value={tareaData.Fcreado}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+
+                    <label className="mb-4">
+                        Titulo:
+                        <input
+                            type="text"
+                            name="titulo"
+                            value={tareaData.titulo}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+
+                    <label className="mb-4">
+                        Descripcion:
+                        <input
+                            type="text"
+                            name="descripcion"
+                            value={tareaData.descripcion}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+
+                    <label className="mb-4">
+                        Estado de la tarea:
+                        <select
+                            name="estado"
+                            value={tareaData.estado}
+                            onChange={handleInputChange}
+                        >
+                            <option value="no_completado">Incompleta</option>
+                            <option value="completado">Completada</option>
+                            <option value="vencida">Vencida</option>
+                        </select>
+                    </label>
+
+                    <button
+                        className='bg-green-400'
+                        type="button"
+                        onClick={handleCreateTarea}
+                    >
+                        Crear Tarea
+                    </button>
+                </form>
             </div>
             <div>
-                {obraId && <TareaList obraId={obraId} />}
+                <table>
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Fecha Creada</th>
+                        <th>Fecha Vencimiento</th>
+                        <th>Fecha Completado</th>
+                        <th>Titulo</th>
+                        <th>Descripcion</th>
+                        <th>Estado de la Tarea</th>
+                        <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tareas.map((tarea) => (
+                        <tr key={tarea.id}>
+                            <td>{tarea.id}</td>
+                            <td>{tarea.Fvence}</td>
+                            <td>{tarea.Fcreado}</td>
+                            <td>{tarea.Fcompletado}</td>
+                            <td>{tarea.titulo}</td>
+                            <td>{tarea.descripcion}</td>
+                            <td>{tarea.estado}</td>
+                            <td>
+                                <button className='bg-orange-400'
+                                onClick={() => handleActualizar(id, tarea.id)}>
+                                    Actualizar
+                                </button>
+                                <button className='bg-red-600'
+                                onClick={() => handleEliminar(id, tarea.id) }
+                                    >
+                                    Eliminar
+                                </button>
+                            </td>
+                            
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
 
         </div>
