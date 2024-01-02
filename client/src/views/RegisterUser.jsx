fb5e902a68f335eb1748fa350ab36a85bac357cd
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-01-02 14:24:27 -0600

    Add vista de registro user con estilos

---
+++ client/src/views/RegisterUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RegisterForm from "../components/RegisterForm";
+
+
+const RegisterUser = () => {
+    return(
+        <div>
+            <h1>Registrarse</h1>
+            <RegisterForm />
+        </div>
+    );
+};
+export default RegisterUser;
