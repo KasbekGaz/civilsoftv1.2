e255d50e3b263ade5db74ce70f99a10e04de45f2
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-01-02 14:14:20 -0600

    Add boton registrar usuario y estilos

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -6,12 +6,16 @@ return (
     <div>
         <h1>Bienvenido a la aplicación</h1>
         <p>¡Comienza tu viaje aquí! Inicia sesion o Registrate.</p>
-        <button className="rounded-full bg-yellow-400 py-2 px-4 mb-4">
+        <button className="hover:bg-yellow-500 rounded-full bg-yellow-400 py-2 px-4 mb-4 ml-4 mr-4">
             <Link to="/login">
                 Iniciar sesión
             </Link>
         </button>
-        
+        <button className="hover:bg-yellow-500 rounded-full bg-yellow-400 py-2 px-4 mb-4 ml-4 mr-4">
+            <Link to="/register">
+                Registrarse
+            </Link>
+        </button>
     </div>
 );
 };
