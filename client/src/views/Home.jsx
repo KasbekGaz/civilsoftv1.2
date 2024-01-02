a6bc017f37b6f81fe9e943a561703a51b9ba3762
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-01-02 11:10:26 -0600

    Agregando un boton a la vista

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -5,8 +5,13 @@ const Home = () => {
 return (
     <div>
         <h1>Bienvenido a la aplicación</h1>
-        <p>¡Comienza tu viaje aquí!</p>
-        <Link to="/login">Iniciar sesión</Link>
+        <p>¡Comienza tu viaje aquí! Inicia sesion o Registrate.</p>
+        <button className="rounded-full bg-yellow-400 py-2 px-4 mb-4">
+            <Link to="/login">
+                Iniciar sesión
+            </Link>
+        </button>
+        
     </div>
 );
 };
