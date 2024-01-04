24c04964d81156f7de686775ba6f78304ef4e12c
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-01-04 12:27:36 -0600

    Configurando media y static files

--- civilsoft_api/settings.py
+++ civilsoft_api/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media/galeria')]
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/galeria')
 
 
 # Default primary key field type
