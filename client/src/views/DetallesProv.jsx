b45a790e4582bb8d95956c5a0f607806662cb6f9
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-01-08 02:08:36 -0600

    Add peticiones create y eliminar con formulario y tabla de banca y materiales

--- client/src/views/DetallesProv.jsx
+++ client/src/views/DetallesProv.jsx
@@ -5,15 +5,30 @@ import APIbackend from "../api/APIbackend";
 
 
 
-const DetallesProv = () =>{
+const DetallesProv = () => {
     const { id } = useParams();
     const [ provData, setProvData ] = useState({});
     const navigate = useNavigate();
+    //! formulario materiales
+    const [ materialData, setMdata ] = useState({
+        proveedor: id,
+        unidad: '',
+        material: '',
+        precio: '',
+    });
     //* Pa la tabla Materiales
     const [ material, setMateriales] = useState([]);
+    //! formulario Banca
+    const [ infobanca, setBancaData ] = useState({
+        proveedor: id,
+        banco: '',
+        clave_banco: '',
+        cuenta_banco: '',
+    });
     //* pa la tabla Banca
     const [ banca, setBanca ] = useState([]);
 
+//! obetener datos del proveedor por su id
     const fetchProvDetails = async () =>{
         try{
             console.log(id);
@@ -56,6 +71,91 @@ const DetallesProv = () =>{
             console.error('Error al listar materiales', error.message);
         }
     };
+    //! Agregar un material a un proveedor
+    const handleInputChange = (e) =>{
+        const { name, value } = e.target;
+        setMdata((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+    const createMaterial = async () =>{
+        try{
+            console.log(id,materialData);
+            const response = await APIbackend.createMaterial(id, materialData);
+            console.log('datos material creado', response);
+            alert('Se Agrego el material correctamente !!');
+
+            //* limpiamos formulario
+            setMdata({
+                proveedor: id,
+                unidad: '',
+                material: '',
+                precio: '',
+            });
+
+            fetchMaterial();
+
+        }catch(error){
+            console.error('Error al agregar el material');
+        }
+    };
+    //! Agregar Banca a un proveedor
+    const handleInputChangeBanca = (e) =>{
+        const { name, value } = e.target;
+        setBancaData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+    const createBanca = async () =>{
+        console.log(id,infobanca);
+            const response = await APIbackend.createBanca(id, infobanca);
+            console.log('datos banca creado', response);
+            alert('Se Agrego la informacion correctamente !!');
+
+            //* limpiamos formulario
+            setBancaData({
+                proveedor: id,
+                banco: '',
+                clave_banco: '',
+                cuenta_banco: '',
+            });
+            
+            fetchBanca();
+    };
+    //! Editar un material
+    const editarMaterial = (id, mId) =>{
+        console.log('Id de material',id, mId);
+        navigate(`/update-material-by-obra/${id}/${mId}`);
+    };
+    //! Editar Banca
+    const editarBanca = (id, bId) =>{
+        console.log('Id de banca',id, bId);
+        navigate(`/update-banca-by-obra/${id}/${bId}`);
+    };
+    //! Eliminar material
+    const eliminarMaterial = async (id,mId) =>{
+        try{
+            console.log('Datos que entran: ', id, mId);
+            await APIbackend.deleteMaterial(id,mId);
+            alert('Eliminaste el material con exito !!');
+            fetchMaterial();
+        }catch(error){
+            console.log('Error al eliminar el material', error.message);
+        }
+    };
+    //! Eliminar Banca
+    const eliminarBanca = async (id,mId) =>{
+        try{
+            console.log('Datos que entran: ', id, mId);
+            await APIbackend.deleteBanca(id,mId);
+            alert('Eliminaste la informacion con exito !!');
+            fetchBanca();
+        }catch(error){
+            console.log('Error al eliminar la banca', error.message);
+        }
+    };
 
 return(
     <div>
@@ -72,8 +172,40 @@ return(
             <h2> RFC: {provData.coderfc} </h2>
             <h2> Descripción: {provData.descripcion} </h2>
         </div>
-        <div className="Materiales">
-            <button className="bg-green-400 py-2 px-4 mb-4 rounded-full">
+        <div className="">
+            <h1>Agregar Materiales ofertados por el Proveedor</h1>
+            <form className="flex flex-col justify-center items-center">
+                <label className="mb-4">
+                    Unidad:
+                    <input 
+                        className="ml-4"
+                        type="text"
+                        name="unidad"
+                        value={materialData.unidad}
+                        onChange={handleInputChange} />
+                </label>
+                <label className="mb-4">
+                    Material:
+                    <input 
+                        className="ml-4"
+                        type="text"
+                        name="material"
+                        value={materialData.material}
+                        onChange={handleInputChange} />
+                </label>
+                <label className="mb-4">
+                    Precio:
+                    <input 
+                        className="ml-4"
+                        type="number"
+                        name="precio"
+                        value={materialData.precio}
+                        onChange={handleInputChange} />
+                </label>
+            </form>
+            <button 
+                className="bg-green-400 py-2 px-4 mb-4 rounded-full"
+                onClick={createMaterial}>
                 Agregar Material
             </button>
             <table>
@@ -94,10 +226,14 @@ return(
                             <td>{m.material}</td>
                             <td>{m.precio}</td>
                             <td>
-                                <button>
+                                <button 
+                                    className="bg-orange-400 m-2 rounded-full py-2 px-3"
+                                    onClick={() => editarMaterial(id, m.id)}>
                                     Editar
                                 </button>
-                                <button>
+                                <button 
+                                    className="bg-red-600 m-2 rounded-full py-2 px-3"
+                                    onClick={() => eliminarMaterial(id, m.id)}>
                                     Eliminar
                                 </button>
                             </td>
@@ -107,9 +243,75 @@ return(
             </table>
         </div>
         <div className="Banca">
-            <button className="bg-green-400 py-2 px-4 mb-4 rounded-full">
+            <h1>Agregar Informacion Bancaria</h1>
+            <form className="flex flex-col justify-center items-center">
+            <label className="mb-4">
+                    Nombre del Banco:
+                    <input 
+                        className="ml-4"
+                        type="text"
+                        name="banco"
+                        value={infobanca.banco}
+                        onChange={handleInputChangeBanca} />
+                </label>
+                <label className="mb-4">
+                    Clave del Banco:
+                    <input 
+                        className="ml-4"
+                        type="text"
+                        name="clave_banco"
+                        value={infobanca.clave_banco}
+                        onChange={handleInputChangeBanca} />
+                </label>
+                <label className="mb-4">
+                    Cuenta de Banco:
+                    <input 
+                        className="ml-4"
+                        type="text"
+                        name="cuenta_banco"
+                        value={infobanca.cuenta_banco}
+                        onChange={handleInputChangeBanca} />
+                </label>
+            </form>
+            <button 
+                className="bg-green-400 py-2 px-4 mb-4 rounded-full"
+                onClick={createBanca}>
                 Agregar Informacion Bancaria
             </button>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th> ID </th>
+                        <th> Banco </th>
+                        <th> Cuenta de Banco </th>
+                        <th> Clave de banco </th>
+                        <th> Acciones </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {banca.map((b) => (
+                        <tr key={b.id} >
+                            <td>{b.id}</td>
+                            <td>{b.banco}</td>
+                            <td>{b.cuenta_banco}</td>
+                            <td>{b.clave_banco}</td>
+                            <td>
+                                <button 
+                                    className="bg-orange-400 m-2 rounded-full py-2 px-3"
+                                    onClick={() => editarBanca(id, b.id)}>
+                                    Editar
+                                </button>
+                                <button 
+                                    className="bg-red-600 m-2 rounded-full py-2 px-3"
+                                    onClick={() => eliminarBanca(id, b.id)}>
+                                    Eliminar
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
         </div>
     </div>
