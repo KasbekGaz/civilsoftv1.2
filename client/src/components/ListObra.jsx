443257550479e2b757bddaa4b0d242a53ee705f0
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-02-16 13:18:28 -0600

    Fix vista de la tabla

--- client/src/components/ListObra.jsx
+++ client/src/components/ListObra.jsx
@@ -88,12 +88,7 @@ return (
                 <tr className="bg-gray-800 border-b">
                 <th scope="col" className="px-6 py-3 text-center rounded-s-2xl">ID</th>
                 <th scope="col" className="px-6 py-3 text-center">Nombre</th>
-                <th scope="col" className="px-6 py-3 text-center">Localidad</th>
-                <th scope="col" className="px-6 py-3 text-center">Municipio</th>
-                <th scope="col" className="px-6 py-3 text-center">Dependencia</th>
                 <th scope="col" className="px-6 py-3 text-center">Fecha Registrado</th>
-                <th scope="col" className="px-6 py-3 text-center">Presupuesto</th>
-                <th scope="col" className="px-6 py-3 text-center">Ver detalles</th>
                 <th scope="col" className="px-6 py-3 text-center rounded-e-2xl">Acciones</th>
                 </tr>
             </thead>
@@ -102,23 +97,17 @@ return (
                 <tr className="bg-gray-600 border-b" key={obra.id}>
                     <td scope="row" className="px-4 py-2 text-white text-center text-base font-semibold">{obra.id}</td>
                     <td scope="row" className="px-4 py-2 text-white text-right text-base font-semibold" >{obra.nombre}</td>
-                    <td scope="row" className="px-4 py-2 text-white text-right text-base font-semibold" >{obra.localidad}</td>
-                    <td scope="row" className="px-4 py-2 text-white text-right text-base font-semibold" >{obra.municipio}</td>
-                    <td scope="row" className="px-4 py-2 text-white text-right text-base font-semibold" >{obra.dependencia}</td>
                     <td scope="row" className="px-4 py-2 text-white text-justify text-base font-semibold">{obra.fecha}</td>
-                    <td scope="row" className="px-4 py-2 text-white text-justify text-base font-semibold">{obra.p_inicial}</td>
-                    <td> 
-                        <button className="text-center font-semibold rounded-full bg-fuchsia-500 py-2 px-4 mb-4 mt-4 hover:bg-fuchsia-600"
+                    <td scope="row" className="px-4 py-2 flex space-x-1 justify-items-center">
+                        <button className="flex-1 text-center font-semibold rounded-full bg-fuchsia-500 py-2 px-4 hover:bg-fuchsia-600"
                         onClick={() => handleDetalles(obra.id)}>
                             Detalles
                         </button>
-                    </td>
-                    <td>
-                        <button className="text-center font-semibold rounded-full bg-orange-500 py-2 px-4 mb-4 mt-4 hover:bg-orange-600"
+                        <button className="flex-1 text-center font-semibold rounded-full bg-orange-500 py-2 px-4  hover:bg-orange-600"
                         onClick={() => handleActualizar(obra.id)}>
                             Actualizar
                         </button>
-                        <button className="text-center font-semibold rounded-full bg-red-500 py-2 px-4 mb-4 mt-4 hover:bg-red-600"
+                        <button className="flex-1 text-center font-semibold rounded-full bg-red-500 py-2 px-4 hover:bg-red-600"
                         onClick={() => handleEliminar(obra.id) }
                             >
                             Eliminar
