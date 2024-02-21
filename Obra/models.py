ef038c6ada4206ab588f4b02ee6f82a8bfe6e1b4
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-02-21 16:02:02 -0600

    Add modelo ABONO para la DB

--- Obra/models.py
+++ Obra/models.py
@@ -290,3 +290,12 @@ class Volumen(models.Model):
 
     def __str__(self):
         return f"{self.codigo} ({self.importe}) ({self.importe_mod}) ({self.diferencia})"
+
+#! Modelo de Abonos
+
+
+class Abono(models.Model):
+    obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    descripcion = models.CharField(max_length=255)
+    importe = models.DecimalField(max_digits=10, decimal_places=2, default=0)
