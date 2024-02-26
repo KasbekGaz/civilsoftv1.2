5dfd79efab57025975f9d60e198870284e1bde3e
KasbekGaz <oswa.laza.flores@hotmail.com>
2024-02-26 14:45:59 -0600

    Fix estilos erroneos

--- client/src/components/Gasto/UpdateGasto.jsx
+++ client/src/components/Gasto/UpdateGasto.jsx
@@ -71,9 +71,9 @@ const UpdateGasto = () =>{
     }
 
 return(
-    <div className=" container mx-4 my-4 max-w-sm p-6 bg-violet-950- border border-violet-600 rounded-lg drop-shadow-xl">
+    <div className="container mx-4 my-4 max-w-sm p-6 bg-violet-950- border border-violet-600 rounded-lg drop-shadow-xl">
 
-        <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Actualizar Tarea: {gastoData.concepto}</h1>
+        <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Actualizar Gasto: {gastoData.concepto}</h1>
 
         <button className="text-center font-semibold rounded-full bg-green-400 hover:bg-green-700 py-2 px-4 mb-4 mt-4" 
         onClick={() => handleBack(id)}>
@@ -172,7 +172,7 @@ return(
                     </label>
 
                     <button 
-                        className="bg-green-400"
+                        className="text-center font-semibold rounded-full bg-yellow-500 py-2 px-4 mb-4 mt-4 hover:bg-green-500"
                         type="button"
                         onClick={handleUpdateGasto}> 
                         Actualizar Gasto
