de9795ddbf3d451e6c6644b02cf11f93573027c0
Aldrin <aldrin10@live.com.mx>
2024-05-28 10:44:42 -0600

    Actualizando la ruta del proyecto

--- start_django.bat
+++ start_django.bat
@@ -1,4 +1,4 @@
 @echo off
-cd C:\Users\oswal\Desktop\Proyectos\Civilsoftv1.2
+cd C:\Users\USUARIO\Desktop\SistemaCivilsoftApp\civilsoftv1.2
 call .\venv\Scripts\activate
 python manage.py runserver 0.0.0.0:8000
